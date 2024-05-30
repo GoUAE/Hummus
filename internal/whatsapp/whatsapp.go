f62a19fbccb078ed199352a635a2f9fba209dbde
Gaurav Gosain <itsgauravgosain@gmail.com>
2024-05-30 13:20:17 +0400

    feat: got stickers working

--- internal/whatsapp/whatsapp.go
+++ internal/whatsapp/whatsapp.go
@@ -3,8 +3,10 @@ package whatsapp
 import (
 	"bytes"
 	"context"
+	"fmt"
 	"log"
 	"os"
+	"os/exec"
 	"os/signal"
 	"regexp"
 	"strings"
@@ -23,6 +25,47 @@ import (
 	waLog "go.mau.fi/whatsmeow/util/log"
 )
 
+func convertWebpToX(inputBuffer *bytes.Buffer, outformat string) (*bytes.Buffer, error) {
+	// Create temporary files for input and output
+	inputFile, err := os.CreateTemp("", "input-*.webp")
+	if err != nil {
+		return nil, fmt.Errorf("failed to create temp input file: %v", err)
+	}
+	defer inputFile.Close()
+
+	outputFile, err := os.CreateTemp("", "output-*."+outformat)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create temp output file: %v", err)
+	}
+	defer outputFile.Close()
+
+	// Write the input buffer to the temporary input file
+	_, err = inputFile.Write(inputBuffer.Bytes())
+	if err != nil {
+		return nil, fmt.Errorf("failed to write to temp input file: %v", err)
+	}
+
+	// Close the input file to flush the data
+	inputFile.Close()
+
+	// Run imagemagick "convert" command to convert the input file to res
+	cmd := exec.Command("convert", inputFile.Name(), outputFile.Name())
+	err = cmd.Run()
+	if err != nil {
+		return nil, fmt.Errorf("magick command failed: %v", err)
+	}
+
+	// Read the output file into a buffer
+	outputData, err := os.ReadFile(outputFile.Name())
+	if err != nil {
+		return nil, fmt.Errorf("failed to read temp output file: %v", err)
+	}
+
+	outputBuffer := bytes.NewBuffer(outputData)
+
+	return outputBuffer, nil
+}
+
 func GetEventHandler(client *whatsmeow.Client) func(interface{}) {
 	return func(event interface{}) {
 		switch v := event.(type) {
@@ -31,181 +74,202 @@ func GetEventHandler(client *whatsmeow.Client) func(interface{}) {
 				jid := v.Info.Chat
 
 				if jid.String() == config.HummusConfig.WhatsappGoUAEJID {
-					// fallback profile picture
-					profilePic := "https://wearedesigners.golang.ae/dist/img/gouae-mascot.png"
+					go func(jid types.JID, client *whatsmeow.Client, v *events.Message) {
+						// fallback profile picture
+						profilePic := "https://wearedesigners.golang.ae/dist/img/gouae-mascot.png"
 
-					userProfilePicInfo, err := client.
-						GetProfilePictureInfo(
-							v.Info.Sender,
-							&whatsmeow.GetProfilePictureParams{},
-						)
-					if err != nil {
-						log.Println(err.Error())
-					}
-
-					if userProfilePicInfo != nil {
-						profilePic = userProfilePicInfo.URL
-					}
-
-					// TODO: check other message types (audio, document, stickers etc.) and handle them properly.
-					imageMessage := v.Message.GetImageMessage()
-					stickerMessage := v.Message.GetStickerMessage()
-					files := []*discordgo.File{}
-					var embeds []*discordgo.MessageEmbed
-
-					var imageEmbed discordgo.MessageEmbedImage
-					if imageMessage != nil {
-
-						mimeType := "image/jpg"
-
-						mimeTypeRef := imageMessage.Mimetype
-						if mimeTypeRef != nil {
-							mimeType = *mimeTypeRef
+						userProfilePicInfo, err := client.
+							GetProfilePictureInfo(
+								v.Info.Sender,
+								&whatsmeow.GetProfilePictureParams{},
+							)
+						if err != nil {
+							log.Println(err.Error())
 						}
 
-						extension, hasExtension := strings.CutPrefix(mimeType, "image/")
-						if !hasExtension {
-							extension = "jpg"
+						if userProfilePicInfo != nil {
+							profilePic = userProfilePicInfo.URL
 						}
 
-						data, _ := client.Download(imageMessage)
-						imageEmbed = discordgo.MessageEmbedImage{
-							URL:    "attachment://image." + extension,
-							Width:  int(v.Message.ImageMessage.GetWidth()),
-							Height: int(v.Message.ImageMessage.GetHeight()),
+						// TODO: check other message types (audio, document, stickers etc.) and handle them properly.
+						imageMessage := v.Message.GetImageMessage()
+						stickerMessage := v.Message.GetStickerMessage()
+						files := []*discordgo.File{}
+						var embeds []*discordgo.MessageEmbed
+
+						var imageEmbed discordgo.MessageEmbedImage
+						if imageMessage != nil {
+
+							mimeType := "image/jpg"
+
+							mimeTypeRef := imageMessage.Mimetype
+							if mimeTypeRef != nil {
+								mimeType = *mimeTypeRef
+							}
+
+							extension, hasExtension := strings.CutPrefix(mimeType, "image/")
+							if !hasExtension {
+								extension = "jpg"
+							}
+
+							data, _ := client.Download(imageMessage)
+							imageEmbed = discordgo.MessageEmbedImage{
+								URL:    "attachment://image." + extension,
+								Width:  int(v.Message.ImageMessage.GetWidth()),
+								Height: int(v.Message.ImageMessage.GetHeight()),
+							}
+							files = append(files, &discordgo.File{
+								Name:        "image." + extension,
+								ContentType: mimeType,
+								Reader:      bytes.NewBuffer(data),
+							})
+							embeds = append(embeds, &discordgo.MessageEmbed{
+								Type:  discordgo.EmbedTypeImage,
+								Image: &imageEmbed,
+							})
 						}
-						files = append(files, &discordgo.File{
-							Name:        "image." + extension,
-							ContentType: mimeType,
-							Reader:      bytes.NewBuffer(data),
-						})
-						embeds = append(embeds, &discordgo.MessageEmbed{
-							Type:  discordgo.EmbedTypeImage,
-							Image: &imageEmbed,
-						})
-					}
 
-					// FIXME: fix sticker messages (maybe save them, convert to .gif from .webp and then attach it?)
-					if stickerMessage != nil {
-						mimeType := "image/webp"
+						if stickerMessage != nil {
+							mimeType := "image/png"
 
-						mimeTypeRef := stickerMessage.Mimetype
-						if mimeTypeRef != nil {
-							mimeType = *mimeTypeRef
-						}
+							mimeTypeRef := stickerMessage.Mimetype
+							if mimeTypeRef != nil {
+								mimeType = *mimeTypeRef
+							}
 
-						extension, hasExtension := strings.CutPrefix(mimeType, "image/")
-						if !hasExtension {
-							extension = "webp"
-						}
+							extension, hasExtension := strings.CutPrefix(mimeType, "image/")
+							if !hasExtension {
+								extension = "png"
+							}
 
-						data, _ := client.Download(stickerMessage)
-						imageEmbed = discordgo.MessageEmbedImage{
-							URL:    "attachment://image." + extension,
-							Width:  int(v.Message.ImageMessage.GetWidth()),
-							Height: int(v.Message.ImageMessage.GetHeight()),
-						}
-						files = append(files, &discordgo.File{
-							Name:        "image." + extension,
-							ContentType: mimeType,
-							Reader:      bytes.NewBuffer(data),
-						})
-						embeds = append(embeds, &discordgo.MessageEmbed{
-							Type:  discordgo.EmbedTypeImage,
-							Image: &imageEmbed,
-						})
-					}
+							data, _ := client.Download(stickerMessage)
 
-					newMessageContent := v.Message.GetConversation() +
-						v.Message.ExtendedTextMessage.GetText() +
-						v.Message.ImageMessage.GetCaption()
+							reader := bytes.NewBuffer(data)
 
-					var quotedMessage *waE2E.Message // Declare the final variable as nil initially
-
-					// Check if v.Message exists and is not nil
-					if msg := v.Message; msg != nil {
-						// Check if v.Message.ExtendedTextMessage exists and is not nil
-						if etm := msg.ExtendedTextMessage; etm != nil {
-							// Check if v.Message.ExtendedTextMessage.ContextInfo exists and is not nil
-							if etmContext := etm.ContextInfo; etmContext != nil {
-								// use these strings to get PushNames
-								userJIDStrings := etmContext.GetMentionedJID()
-
-								var userJIDs []types.JID
-
-								// find actual JIDs
-								for _, userJIDString := range userJIDStrings {
-									jid, err := types.ParseJID(userJIDString)
-									if err == nil {
-										userJIDs = append(userJIDs, jid)
-									}
+							if stickerMessage.GetIsAnimated() {
+								mimeType = "image/gif"
+								extension = "gif"
+								reader, err = convertWebpToX(reader, "gif")
+								if err != nil {
+									log.Println("Failed to convert to gif", err.Error())
 								}
-
-								// Regex pattern with capturing mention
-								regex := regexp.MustCompile(`(?i)@([a-zA-Z0-9]+)`)
-
-								// fmt.Println(userInfoMap)
-
-								for _, userJID := range userJIDs {
-									// fallback
-									replaceWith := "`@[REDACTED]`"
-									user, err := client.Store.Contacts.GetContact(userJID)
-									if err == nil {
-										replaceWith = user.PushName
-									}
-									newMessageContent = regex.ReplaceAllString(newMessageContent, "`"+replaceWith+"`")
+							} else {
+								reader, err = convertWebpToX(reader, "png")
+								if err != nil {
+									log.Println("Failed to convert to gif", err.Error())
 								}
+							}
 
-								// Finally, access the GetQuotedMessage() method
-								quotedMessage = etmContext.GetQuotedMessage()
+							imageEmbed = discordgo.MessageEmbedImage{
+								URL:    "attachment://image." + extension,
+								Width:  int(v.Message.ImageMessage.GetWidth()),
+								Height: int(v.Message.ImageMessage.GetHeight()),
+							}
+							files = append(files, &discordgo.File{
+								Name:        "image." + extension,
+								ContentType: mimeType,
+								Reader:      reader,
+							})
+							embeds = append(embeds, &discordgo.MessageEmbed{
+								Type:  discordgo.EmbedTypeImage,
+								Image: &imageEmbed,
+							})
+						}
+
+						newMessageContent := v.Message.GetConversation() +
+							v.Message.ExtendedTextMessage.GetText() +
+							v.Message.ImageMessage.GetCaption()
+
+						var quotedMessage *waE2E.Message // Declare the final variable as nil initially
+
+						// Check if v.Message exists and is not nil
+						if msg := v.Message; msg != nil {
+							// Check if v.Message.ExtendedTextMessage exists and is not nil
+							if etm := msg.ExtendedTextMessage; etm != nil {
+								// Check if v.Message.ExtendedTextMessage.ContextInfo exists and is not nil
+								if etmContext := etm.ContextInfo; etmContext != nil {
+									// use these strings to get PushNames
+									userJIDStrings := etmContext.GetMentionedJID()
+
+									var userJIDs []types.JID
+
+									// find actual JIDs
+									for _, userJIDString := range userJIDStrings {
+										jid, err := types.ParseJID(userJIDString)
+										if err == nil {
+											userJIDs = append(userJIDs, jid)
+										}
+									}
+
+									// Regex pattern with capturing mention
+									regex := regexp.MustCompile(`(?i)@([a-zA-Z0-9]+)`)
+
+									// fmt.Println(userInfoMap)
+
+									for _, userJID := range userJIDs {
+										// fallback
+										replaceWith := "`@[REDACTED]`"
+										user, err := client.Store.Contacts.GetContact(userJID)
+										if err == nil {
+											replaceWith = user.PushName
+										}
+										newMessageContent = regex.ReplaceAllString(newMessageContent, "`"+replaceWith+"`")
+									}
+
+									// Finally, access the GetQuotedMessage() method
+									quotedMessage = etmContext.GetQuotedMessage()
+								}
 							}
 						}
-					}
 
-					replyContent := ""
+						replyContent := ""
 
-					if quotedMessage != nil {
-						replyContent = quotedMessage.GetConversation() +
-							quotedMessage.ExtendedTextMessage.GetText() +
-							quotedMessage.ImageMessage.GetCaption()
+						if quotedMessage != nil {
+							replyContent = quotedMessage.GetConversation() +
+								quotedMessage.ExtendedTextMessage.GetText() +
+								quotedMessage.ImageMessage.GetCaption()
 
-						lines := strings.Split(replyContent, "\n")
+							lines := strings.Split(replyContent, "\n")
 
-						// Iterate through each line and add the suffix
-						var processedLines []string
-						for _, line := range lines {
-							processedLine := "> " + line
-							processedLines = append(processedLines, processedLine)
+							// Iterate through each line and add the suffix
+							var processedLines []string
+							for _, line := range lines {
+								processedLine := "> " + line
+								processedLines = append(processedLines, processedLine)
+							}
+
+							// Join the processed lines back with newline separators
+							replyContent = strings.Join(processedLines, "\n") + "\n\n"
 						}
 
-						// Join the processed lines back with newline separators
-						replyContent = strings.Join(processedLines, "\n") + "\n\n"
-					}
+						messageContent := replyContent + newMessageContent
 
-					messageContent := replyContent + newMessageContent
+						// Regex pattern with capturing mention
+						regex := regexp.MustCompile(`(?i)@([a-zA-Z0-9]+)`)
 
-					// Regex pattern with capturing mention
-					regex := regexp.MustCompile(`(?i)@([a-zA-Z0-9]+)`)
+						// FIXME: Replace mentions in replyContent with redacted for now
+						// (until we figure out how to get the JID and eventually name for each mention)
+						redactedText := regex.ReplaceAllString(messageContent, "`@[REDACTED]`")
 
-					// FIXME: Replace mentions in replyContent with redacted for now
-					// (until we figure out how to get the JID and eventually name for each mention)
-					redactedText := regex.ReplaceAllString(messageContent, "`@[REDACTED]`")
-
-					_, err = config.DG.WebhookExecute(
-						config.HummusConfig.DiscordWebhookID,
-						config.HummusConfig.DiscordWebhoolToken,
-						true,
-						&discordgo.WebhookParams{
-							Content:   redactedText,
-							Username:  v.Info.PushName,
-							AvatarURL: profilePic,
-							Embeds:    embeds,
-							Files:     files,
-						},
-					)
-					if err != nil {
-						log.Println(err.Error())
-					}
+						if redactedText == "" && embeds == nil {
+							return
+						}
+						_, err = config.DG.WebhookExecute(
+							config.HummusConfig.DiscordWebhookID,
+							config.HummusConfig.DiscordWebhoolToken,
+							true,
+							&discordgo.WebhookParams{
+								Content:   redactedText,
+								Username:  v.Info.PushName,
+								AvatarURL: profilePic,
+								Embeds:    embeds,
+								Files:     files,
+							},
+						)
+						if err != nil {
+							log.Println(err.Error())
+						}
+					}(jid, client, v)
 				}
 			}
 		}
